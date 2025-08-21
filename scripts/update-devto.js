a870dacdf785f995f87d9b70d084c73b60b0b65d
Dainy Jose <103561567+dainyjose@users.noreply.github.com>
2025-08-21 12:36:09 +0530

    Created update-devto.js

---
+++ scripts/update-devto.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+const username = "dainy_jose";
+const readmePath = "./README.md";
+
+async function updateDevtoSection() {
+  const res = await fetch(`https://dev.to/api/articles?username=${username}&per_page=5`);
+  const articles = await res.json();
+
+  let devtoSection = "## 📝 Latest Dev.to Articles\n\n";
+  articles.forEach(article => {
+    devtoSection += `- [${article.title}](${article.url})\n`;
+  });
+  devtoSection += `\n[See all articles on Dev.to →](https://dev.to/${username})\n`;
+
+  let readme = fs.readFileSync(readmePath, "utf-8");
+
+  const newReadme = readme.replace(
+    /<!-- DEVTO-START -->[\s\S]*<!-- DEVTO-END -->/,
+    `<!-- DEVTO-START -->\n${devtoSection}\n<!-- DEVTO-END -->`
+  );
+
+  fs.writeFileSync(readmePath, newReadme, "utf-8");
+  console.log("README updated with latest Dev.to articles!");
+}
+
+updateDevtoSection();
